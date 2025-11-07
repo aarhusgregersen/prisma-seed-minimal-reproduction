741e22b89b5e24841e369f12283a1f5bdcd7fc7c
Martin Aarhus Gregersen <marting12@live.dk>
2025-11-07 11:41:20 +0100

    add right script to public repo

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -4,6 +4,6 @@ import type { PrismaConfig } from "prisma";
 export default {
   schema: path.join("prisma", "schema"),
   migrations: {
-    seed: "pnpm exec ls",
+    seed: "pnpm exec tsx packages/seed/src/index.ts",
   },
 } satisfies PrismaConfig;
