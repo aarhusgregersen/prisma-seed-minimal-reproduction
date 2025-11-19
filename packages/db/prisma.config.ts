111cd7d54a34ee3366be984705d10a21a22a62b9
Martin Aarhus Gregersen <marting12@live.dk>
2025-11-19 16:39:52 +0100

    attempt ot update to prisma 7 to verify issue

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,9 +1,13 @@
 import path from "node:path";
 import type { PrismaConfig } from "prisma";
+import "dotenv/config";
 
 export default {
   schema: path.join("prisma", "schema"),
   migrations: {
     seed: "pnpm exec tsx packages/seed/src/index.ts",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL as string,
   },
 } satisfies PrismaConfig;
